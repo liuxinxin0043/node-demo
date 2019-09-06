47a86f794c4419e423f70a510dff4af0b62a7afb
liuxinxin <2861706215@qq.com>
2019-09-06 15:27:20 +0800

    work

---
+++ 06-buffer/03-data-uri.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs=require('fs'),
+      log=console.log;
+var data=fs.readFileSync('./qr-code.jpg').toString('base64');
+//log(data);
+
+var html
+http.createServer((req,res)=>{
+
+}).listen(8080);
+
