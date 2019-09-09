ccea29ebcef3d87d8aa018d1f0c6b4cf50afe579
liuxinxin <2861706215@qq.com>
2019-09-09 20:26:21 +0800

    work

--- 06-buffer/02-decode.js
+++ 06-buffer/02-decode.js
@@ -1,11 +1,26 @@
 #!/usr/bin/node
 
-const log=console.log;
+//const log=console.log;
 
-var data=process.argv[2];
-var buf=new Buffer(process.argv[2],'base64'),
-    info=buf.toString('utf8').split(':');
+//var data=process.argv[2];
+//var buf=new Buffer(process.argv[2],'base64'),
+  //  info=buf.toString('utf8').split(':');
 
-log('usr:%s,pwd:%s',info[0],info[1]);
+//log('usr:%s,pwd:%s',info[0],info[1]);
+if(process.argv.length !== 3) {
+    console.error('命令行格式：cmd base64_string');
+      process.exit(1);
 
+}
+
+const buf = new Buffer(process.argv[2], 'base64');
+const info = buf.toString('utf8').split(':');
+
+if(info.length !== 2) {
+    console.error('信息有误！');
+      process.exit(2);
+
+}
+
+console.log('user name: %s\npassword: %s', info[0], info[1]);
 
--- 06-buffer/03-data-uri.js
+++ 06-buffer/03-data-uri.js
@@ -1,8 +1,25 @@
 #!/usr/bin/node
 const fs=require('fs'),
       log=console.log,
-      http=require('http');
-var data=fs.readFileSync('./qr-code.jpg').toString('base64');
+      http=require('http'),
+      path=require('path'),
+      file=process.argv[2];
+
+if(process.argv.length !== 3) {
+    console.error('命令行参数格式：cmd fileName');
+      process.exit(1);
+
+}
+
+try {
+    var data = fs.readFileSync(file).toString('base64');
+
+} catch(e) {
+    console.error(e.message);
+      process.exit(2);
+}
+var ext  = path.extname(file);
+//var data=fs.readFileSync('./qr-code.jpg').toString('base64');
 
 var html=''
   + '<!DOCTYPE html>'
@@ -17,5 +34,7 @@ var html=''
   + '</html>'
 
   http.createServer((req,res)=>{
+    console.log(req.headers);
+    console.log(req.url + '\n');
     res.end(html);
   }).listen(8080);
