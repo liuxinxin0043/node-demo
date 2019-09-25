ce7ca8161311ce1bb09d95c484b91bcd7bb02f44
liuxinxin <2861706215@qq.com>
2019-09-25 17:13:14 +0800

    work

---
+++ 12-child-process/02-child.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+console.log('I am a child process id:%d',process.pid);
+
+global.setInterval(function(){
+  console.log('Time is %d',Date.now());
+},2000);
+global.setTimeout(function(){
+  console.log('bye father');
+  process.exit(1);
+},16000);
