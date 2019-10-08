ee6d0b5e862d5797c06004c7dd7aae4c88e8f7d0
liuxinxin <2861706215@qq.com>
2019-10-08 16:21:07 +0800

    work

---
+++ 16-http-server/02-url-parse.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+  const http = require('http'),
+        url  = require('url'),
+        qs   = require('querystring')
+        log  = console.log;
+
+  http.createServer((req,res)=>{
+    log('request URL',req.url);
+
+    //var addr=url.parse(req.url);
+    var addr=url.parse('https://wangding:1234@www.baidu.com:8000/a/b/c?age=28&color=green#/def/efg')
+    log('path-name',addr.pathname);
+    log('auth:',addr.auth);
+    log('username',addr.username);
+    log('password',addr.password);
+    log('host',addr.host);
+    log('port',addr.port);
+    log('path parse',addr.pathname.split('/'));
+    log('query string',addr.query);
+    log('qs parse',qs.parse(addr.query));
+    log('hash',addr.hash)
+    res.end('ok!');
+  }).listen(8080);
